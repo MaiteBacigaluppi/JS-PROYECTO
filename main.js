cb71ed47db70ce2f00d709c258c42bb478e7a459
Bacigaluppi Maite <maite.bacigaluppi.code@gmail.com>
2024-07-23 05:38:19 -0300

    progreso

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 
-let confirmar = confirm ("¿Te gustaría viajar a Ushuaia?");
+function Viaje() {
+const confirmar = confirm ("¿Te gustaría viajar a Ushuaia?");
 
-let respuesta = prompt ("¡Genial! ¿Cómo te llamas?");
-let edad = prompt ("¿Sos mayor de edad?");
+if (confirmar) {
+    console.log ("Buenisimo. ¡Sigamos entonces!"); 
+} else { 
+    alert ("¡No hay problema! ¡Nos vemos la próxima!"); return;
+}
 
-alert ("¡Nos vemos pronto! ¡Buen Viaje!" + " " +   
- respuesta );
+let respuesta;
+do { respuesta = prompt("¡Genial! ¿Cómo te llamas?");
+if (respuesta === null || respuesta.trim() === "") {
+    alert("El nombre no puede estar vacío. Por favor, intenta de nuevo.");
+}
+} while (respuesta === null || respuesta.trim() === "");
+
+let edad = parseInt (prompt ("¿Cúantos años tenes?"));
+
+if (!isNaN(edad) && edad >= 18) {console.log ("Podemos seguir");
+alert ("¡Nos vemos pronto! ¡Buen Viaje!" + " " + respuesta ); 
+} else { 
+    alert ("Necesitas autorizacion de tu padre, madre o tutor. ¡Hasta la proxima"); return;
+ }
+}
+Viaje ();
