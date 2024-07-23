d2eaec262b87b64f75f81ee9e653b5d58a1341ea
Bacigaluppi Maite <maite.bacigaluppi.code@gmail.com>
2024-07-23 03:15:18 -0300

    en proceso

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
-let confirmar = confirm ("¿Te gustaría viajar a Ushuaia?") ;
+let confirmar = confirm ("¿Te gustaría viajar a Ushuaia?");
 
 let respuesta = prompt ("¡Genial! ¿Cómo te llamas?");
 let edad = prompt ("¿Sos mayor de edad?");
 
-alert ("¡Nos vemos pronto! ¡Buen Viaje!"  + " "respuesta);
+alert ("¡Nos vemos pronto! ¡Buen Viaje!" + " " +   
+ respuesta );
